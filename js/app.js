b98d379c419dee53eaef4b62d0e2d8b5c174da9e
Kevan Nguyen <kevanhn@uci.edu>
2017-08-13 08:21:29 -0700

    Added responsiveness to grid/item view in experience / projects page.

--- js/app.js
+++ js/app.js
@@ -15,14 +15,25 @@ function setItemSize() {
         var imgWidth = $(item).children("img").outerWidth(true),
             itemWidth = $(item).outerWidth(true),
             newDescriptionWidth = itemWidth - imgWidth;
-        $(item).children("ul").width(newDescriptionWidth);
+        
+        // Only set new description width if greater than 480 (typical phone size)
+        if ($(window).width() <= 480) 
+            $(item).children("ul").width(itemWidth);
+        else
+            $(item).children("ul").width(newDescriptionWidth);
 
         // Set item height
         var headerHeight = $(item).children(".item-header").outerHeight(true),
             descriptionHeight = $(item).children("ul").outerHeight(true),
             imgHeight = $(item).children("img").outerHeight(true),
             newItemHeight = headerHeight;
-        newItemHeight += (imgHeight > descriptionHeight) ? imgHeight : descriptionHeight;
+
+        // Set item height to height of all 3 (header, description, img) if
+        // viewing on small viewport width of 480 (typical phone size)
+        if ($(window).width() <= 480)
+            newItemHeight += imgHeight + descriptionHeight;
+        else
+            newItemHeight += (imgHeight > descriptionHeight) ? imgHeight : descriptionHeight;
 
         $(item).height(newItemHeight);
     });
